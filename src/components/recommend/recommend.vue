02b046ee6ed02003cf46cf02679af42f4ae04f0f
liuxinhui <916510655@qq.com>
2018-11-21 23:15:52 +0800

    23r

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,10 +1,16 @@
 <template>
    <div>
-      <ul v-for="item in recommend" :key="item.id">
-         <li>
-            <img :src="item.picUrl" />
-         </li>
-      </ul>   
+      <div class="wrapper">
+         <!-- v-if="recommend.length>1" 就可以循环播放了 -->
+         <swiper v-if="recommend.length>1" :options="swiperOption" ref="mySwiper">
+            <!-- slides -->
+            <swiper-slide v-for="item of recommend" :key="item.id">
+               <img class="swiper-img" :src="item.picUrl" /> 
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+         </swiper>  
+      </div>
    </div>
 </template>
 
@@ -15,7 +21,17 @@ import { ERR_OK } from 'api/config'
 export default {
    data() {
       return {
-         recommend:[]
+         recommend:[],
+         swiperOption: {
+            pagination: {
+               el: '.swiper-pagination',
+               type: 'bullets'
+            },
+            autoplay: true,
+            speed: 400,
+            loop:true,
+         },
+         swiperSlides: [1, 2, 3, 4, 5]
       }
    },
    created() {
@@ -35,5 +51,14 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+   .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 40%
+    background: #eee
+    .swiper-img
+      width: 100%
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
