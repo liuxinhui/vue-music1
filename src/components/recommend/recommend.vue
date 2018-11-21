fe9d26d9da595e44811cfba9a14734b5b67fa695
liuxinhui <916510655@qq.com>
2018-11-21 22:23:31 +0800

    1

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,39 @@
 <template>
    <div>
-    recommend
+      <ul v-for="item in recommend" :key="item.id">
+         <li>
+            <img :src="item.picUrl" />
+         </li>
+      </ul>   
    </div>
 </template>
 
 <script>
-export default {}
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
+export default {
+   data() {
+      return {
+         recommend:[]
+      }
+   },
+   created() {
+      this._getRecommend()
+   },
+   methods: {
+      _getRecommend() {
+         getRecommend().then((res) =>{
+            if (res.code === ERR_OK) {
+               this.recommend = res.data.slider
+               console.log(this.recommend)
+            }
+         })
+      }
+   }
+}
 </script>
+
+<style lang="stylus" scoped>
+
+</style>
